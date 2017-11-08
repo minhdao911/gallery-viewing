b25dcc6ca5abe552a765d69963bce76bd670dbe4
Minh Dao <daominh91197@gmail.com>
2017-11-08 22:57:32 +0000

    add functions for popup zooming and modify data retreiving for json file

--- zoom-and-show.js
+++ zoom-and-show.js
@@ -1,263 +1,132 @@
 var width = screen.width,
     height = screen.height,
+    container_width = 0.8 * width,
+    container_height = 0.8 * height,
     rowIndex = 0,
     colIndex = -1,
     radius = 1.8,
     x=-1, y=0,
-    play = false;
+    play = false,
+    scaleMain = 1, scaleSub = 1,
+    currentIndex = 0;
     
-var data = [
-    {
-        image: "../image/Testikuva1.jpg",
-        artist: "Lorem ipsum",
-        description: "Sed orci odio, tempus non euismod id, venenatis vitae purus. Donec faucibus ex ac sapien dapibus tristique."
-    },
-    {
-        image: "../image/Testikuva2.jpg",
-        artist: "Pellentesque",
-        description: "Quisque elementum porttitor nulla ut ultricies. Quisque at ante diam. Maecenas bibendum, lorem vel fringilla cursus, ex ligula sodales ipsum, eu imperdiet lacus lorem eget lorem."
-    },
-    {
-        image: "../image/Testikuva1.jpg",
-        artist: "Lorem ipsum",
-        description: "Sed orci odio, tempus non euismod id, venenatis vitae purus. Donec faucibus ex ac sapien dapibus tristique."
-    },
-    {
-        image: "../image/Testikuva2.jpg",
-        artist: "Pellentesque",
-        description: "Quisque elementum porttitor nulla ut ultricies. Quisque at ante diam. Maecenas bibendum, lorem vel fringilla cursus, ex ligula sodales ipsum, eu imperdiet lacus lorem eget lorem."
-    },
-    {
-        image: "../image/Testikuva1.jpg",
-        artist: "Lorem ipsum",
-        description: "Sed orci odio, tempus non euismod id, venenatis vitae purus. Donec faucibus ex ac sapien dapibus tristique."
-    },
-    {
-        image: "../image/Testikuva2.jpg",
-        artist: "Pellentesque",
-        description: "Quisque elementum porttitor nulla ut ultricies. Quisque at ante diam. Maecenas bibendum, lorem vel fringilla cursus, ex ligula sodales ipsum, eu imperdiet lacus lorem eget lorem."
-    },
-    {
-        image: "../image/Testikuva1.jpg",
-        artist: "Lorem ipsum",
-        description: "Sed orci odio, tempus non euismod id, venenatis vitae purus. Donec faucibus ex ac sapien dapibus tristique."
-    },
-    {
-        image: "../image/Testikuva2.jpg",
-        artist: "Pellentesque",
-        description: "Quisque elementum porttitor nulla ut ultricies. Quisque at ante diam. Maecenas bibendum, lorem vel fringilla cursus, ex ligula sodales ipsum, eu imperdiet lacus lorem eget lorem."
-    },
-    {
-        image: "../image/Testikuva1.jpg",
-        artist: "Lorem ipsum",
-        description: "Sed orci odio, tempus non euismod id, venenatis vitae purus. Donec faucibus ex ac sapien dapibus tristique."
-    },
-    {
-        image: "../image/Testikuva2.jpg",
-        artist: "Pellentesque",
-        description: "Quisque elementum porttitor nulla ut ultricies. Quisque at ante diam. Maecenas bibendum, lorem vel fringilla cursus, ex ligula sodales ipsum, eu imperdiet lacus lorem eget lorem."
-    },
-    {
-        image: "../image/Testikuva1.jpg",
-        artist: "Lorem ipsum",
-        description: "Sed orci odio, tempus non euismod id, venenatis vitae purus. Donec faucibus ex ac sapien dapibus tristique."
-    },
-    {
-        image: "../image/Testikuva2.jpg",
-        artist: "Pellentesque",
-        description: "Quisque elementum porttitor nulla ut ultricies. Quisque at ante diam. Maecenas bibendum, lorem vel fringilla cursus, ex ligula sodales ipsum, eu imperdiet lacus lorem eget lorem."
-    }
-];
+var data, points, point, groups, square;
 
-var numberOfImgs = data.length,
+var numberOfImgs,
     numberOfImgsInARow = 30,
     imgWidth = width/numberOfImgsInARow;
 
-var points = d3.range(numberOfImgs).map(pointMap()),
-    point = points.pop();
-
-var btn = document.querySelector("button");
-
-var svg = d3.select("body").append("svg")
+var playBtn = $(".play"),
+    mapContainer = $(".map");
+    
+var svgMain = d3.select(".map").append("svg")
     .attr("width", width)
     .attr("height", height);
     
-var g = svg.append("g");
+var defs = svgMain.append('svg:defs');
 
-var groups = g.selectAll("g")
-	.data(data)
-	.enter()
-    	.append("g")
-        .attr("class", "gbar");
-
-var image = groups.append("svg:image")
-        .attr("xlink:href", function(d) {return d.image; })
-        .attr("width", imgWidth)
-        .attr("height", imgWidth)
-        .attr("x", function(d, i) {
-            colIndex++;
-            if(i%numberOfImgsInARow==0){
-                colIndex = 0;
-            }
-            return colIndex*imgWidth;
-        })
-        .attr("y", function(d, i) {
-            if(i!==0 && i%numberOfImgsInARow===0){
-                rowIndex+=1;
-            }
-            return rowIndex*imgWidth;
+var gMain = svgMain.append("g");
+    
+d3.json("data.json", function(err, d) {
+    if(err){
+        console.log(err);
+    }else{
+        data = d;
+        numberOfImgs = d.length;
+        data.forEach(function(d, i) {
+          defs.append("svg:pattern")
+            .attr("id", "img" + i)
+            .attr("width", imgWidth) 
+            .attr("height", imgWidth)
+            .attr("patternUnits", "userSpaceOnUse")
+            .append("svg:image")
+            .attr("xlink:href", d.image)
+            .attr("width", imgWidth*1.04)
+            .attr("height", imgWidth*1.04)
+            .attr("x", -0.04*imgWidth/2)
+            .attr("y", -0.04*imgWidth/2);
         });
-
-var circles = groups.append("circle")
-                .attr("cx", function(d,i) {
-                    if(i===0){
-                        colIndex = -1;
-                    }
-                    colIndex++;
-                    if(i%numberOfImgsInARow===0)
-                        colIndex = 0;
-                    return colIndex*imgWidth+imgWidth-4;
-                })
-                .attr("cy", function(d,i){
-                    if(i===0)
-                        rowIndex = 0;
-                    else if(i%numberOfImgsInARow===0)
-                        rowIndex++;
-                    return rowIndex*imgWidth + imgWidth-4;
-                })
-                .attr("r", radius)
-                .attr("class", "hidden")
-                .attr("fill", "rgba(0, 0, 0, 0.7)");
-
-var up = groups.append("text")
-                .attr("x", function(d,i) {
-                    if(i===0){
-                        colIndex = -1;
-                    }
-                    colIndex++;
-                    if(i%numberOfImgsInARow===0)
-                        colIndex = 0;
-                    return colIndex*imgWidth+imgWidth-4;
-                })
-                .attr("y", function(d,i) {
-                    if(i===0)
-                        rowIndex = 0;
-                    else if(i%numberOfImgsInARow===0)
-                        rowIndex++;
-                    return rowIndex*imgWidth + imgWidth-4;
-                })
-                .attr("dy", ".55em")
-                .attr("text-anchor", "middle")
-                .attr("fill", "white")
-                .style("font-size", "0.3em")
-                .attr("class", "hidden")
-                .text("ˆ"); 
-
-var foreignObjects = groups.append("foreignObject")
-    .attr("x",function ( d , i ) { 
-        if(i===0){
-            colIndex = -1;
-        }
-        colIndex++;
-        if(i%numberOfImgsInARow===0)
-            colIndex = 0;
-        return colIndex*imgWidth;
-    })
-    .attr("y",function (d, i) { 
-        if(i===0)
-            rowIndex = 0;
-        else if(i%numberOfImgsInARow===0)
-            rowIndex++;
-        return rowIndex*imgWidth + imgWidth;
-    })
-    .attr("width",function ( d, i ) { return imgWidth } )
-    .attr("height",function ( d, i ) { return 0 } )
-    .attr("class", "unactive");
-
-var htmlDOMs = foreignObjects.append("xhtml:body")
-    .style("margin",0)
-    .style("padding", 0)
-    .style("height", "100%");
-
-var htmlLabels = htmlDOMs.append("div")
-    .attr("class","metadata");
-    
-var downs = htmlLabels.append("i")
-    .attr("class", "fa fa-angle-down");
-
-htmlLabels.append("p")
-    .attr("class","artist")
-    .text(function(d) { return d.artist });
-
-htmlLabels.append("p")
-    .attr("class","description")
-    .html(function(d) { 
-        return d.description; 
-    });
-    
-circles.on("click", function(){
-   var obj = d3.select(this.parentNode).select("foreignObject");
-   var text = d3.select(this.parentNode).select("text");
-    obj.attr("class","active")
-        .transition()
-        .duration(500)
-        .ease(d3.easeLinear)
-        .attr("y", obj.attr("y") - imgWidth/2)
-        .attr("height", imgWidth/2);
-    d3.select(this).attr("class", "hidden");
-    text.attr("class", "hidden");
+        points = d3.range(numberOfImgs).map(pointMap()),
+        point = points.pop();
+        
+        groups = gMain.selectAll("g")
+        	.data(data)
+        	.enter()
+            	.append("g")
+                .attr("class", "gbar");
+        
+        square = groups.append("rect")
+            .attr("x", function(d, i){
+                colIndex++;
+                if(i%numberOfImgsInARow==0){
+                    colIndex = 0;
+                }
+                return colIndex*imgWidth;
+            })
+            .attr("y", function(d, i){
+                if(i!==0 && i%numberOfImgsInARow===0){
+                    rowIndex+=1;
+                }
+                return rowIndex*imgWidth;
+            })
+            .attr("width", imgWidth)
+            .attr("height", imgWidth)
+            .style("fill", function(d, i) {
+                return "url(#img" + i + ")";
+            })
+            .on("click", function(d, i){ // d is the data, i is the index of that data in the data array
+                currentIndex = i;
+                $(".main").addClass("hidden");
+                $(".popup-container").removeClass("hidden");
+                showImgAndData(d);
+            });
+    }
 });
 
-downs.on("click", function(){
-    var obj = d3.select(this.parentNode.parentNode.parentNode);
-    var grp = d3.select(this.parentNode.parentNode.parentNode.parentNode);
-    var text = grp.select("text");
-    var circle = grp.select("circle");
-    console.log(obj);
-    obj.attr("class","unactive")
-        .transition()
-        .duration(500)
-        .ease(d3.easeLinear)
-        .attr("y", imgWidth/2+parseInt(obj.attr("y")))
-        .attr("height", 0);
-    text.attr("class", "show");
-    circle.attr("class", "show");
-});
-
-var zoom = d3.zoom().scaleExtent([1, 60])
+var zoomMain = d3.zoom().scaleExtent([1, 60])
     .on("zoom", zoom);
 
-svg.call(zoom);
+svgMain.call(zoomMain);
         
-btn.addEventListener("click", function(){
-  btn.classList.toggle("play")
+playBtn.on("click", function(){
+//   playBtn.classList.toggle("play")
   play = !play;
   if(play){
-    svg
-      .call(zoom.transform, transform)
+    svgMain
+      .call(zoomMain.transform, transform)
       .call(transition);
   }else{
-      svg.call(zoom);
+      svgMain.call(zoomMain);
   }
 });
 
-svg.on("wheel", function(){ play = false; });
-svg.on("mousedown", function() { play = false; });
-        
+var zoomIn = d3.select(".zoom-in")
+    .on("click", function(){
+        scaleMain *= 2;
+        svgMain.transition()
+          .duration(750)
+          .call(zoomMain.scaleTo, scaleMain);
+    });
+   
+var zoomOut = d3.select(".zoom-out")
+    .on("click", function(){
+        scaleMain *= 1/2;
+        svgMain.transition()
+          .duration(750)
+          .call(zoomMain.scaleTo, scaleMain);
+    });
+
+svgMain.on("wheel", function(){ play = false; });
+svgMain.on("mousedown", function() { play = false; });
+
 function zoom() {
-  g.attr("transform", d3.event.transform);
-  var transform = g.attr("transform");
-  var indexStart = transform.indexOf("scale");
-  var scale = transform.substring(indexStart+6, transform.length-1);
-  if(!play){
-      if(scale>=12 && scale <= 20){
-          d3.selectAll("circle").attr("class", "show");
-          d3.selectAll("text").attr("class", "show");
-      }else{
-          d3.selectAll("circle").attr("class", "hidden");
-          d3.selectAll("text").attr("class", "hidden");
-      }
-  }
+    if($(".main").hasClass("hidden")){
+        gSub.attr("transform", d3.event.transform);
+        scaleSub = d3.event.transform.k;
+    }else{
+        gMain.attr("transform", d3.event.transform);
+        scaleMain = d3.event.transform.k;
+    }
 }
 
 function transform() {
@@ -280,7 +149,7 @@ function transition(svg) {
   svg.transition()
       .delay(3500)
       .duration(10000)
-      .call(zoom.transform, transform)
+      .call(zoomMain.transform, transform)
       .on("end", function() { 
         if(play){
             svg.call(transition); 
@@ -297,4 +166,144 @@ function pointMap() {
       y*imgWidth+imgWidth/2
     ];
   };
+}
+
+// popup logic
+
+var imgContainer = $(".img-container"),
+    poem = $(".poem"),
+    metadata = $(".metadata"),
+    image = $(".image");
+    
+var imgContainerWidth = imgContainer.width(),
+    imgContainerHeight = imgContainer.height();
+
+var svgSub = d3.select(".img-container")
+        .append("svg")
+        .attr("class", "hidden")
+        .attr("width", imgContainerWidth)
+        .attr("height", imgContainerWidth);
+        
+var gSub = svgSub.append("g");
+
+var zoomSub = d3.zoom().scaleExtent([1, 3])
+    .on("zoom", zoom);
+
+var img = gSub.append("svg:image")
+        // .attr("xlink:href", data[i].image)
+        .attr("width", imgContainerWidth)
+        .attr("height", imgContainerWidth)
+        .attr("x", 0)
+        .attr("y", 0);
+        
+image.on("click", function(){
+    $(this).addClass("hidden");
+    svgSub.classed("hidden", false);
+    imgContainer.addClass("full-size");
+    imgContainerWidth = imgContainer.width();
+    svgSub.attr("width", imgContainerWidth);
+    svgSub.attr("height", imgContainerWidth);
+    img.attr("width", imgContainerWidth);
+    img.attr("height", imgContainerWidth);
+    poem.addClass("hidden");
+    metadata.addClass("hidden");
+    $(".zoombtn").removeClass("hidden");
+    $(".closebtn").removeClass("hidden");
+    $(".arrowbtn").addClass("hidden");
+    $(".backbtn").addClass("hidden");
+    svgSub.call(zoomSub);
+    svgSub.call(zoomSub.scaleTo, 1);
+    gSub.attr("transform", "translate(0,0)");
+});
+
+$(".closebtn").on("click", function(){
+  $(this).addClass("hidden");
+  image.removeClass("hidden");
+  svgSub.classed("hidden", true);
+  imgContainer.removeClass("full-size");
+  imgContainerWidth = imgContainer.width();
+  svgSub.attr("width", imgContainerWidth);
+  svgSub.attr("height", imgContainerHeight);
+  img.attr("width", imgContainerWidth);
+  img.attr("height", imgContainerHeight);
+  poem.removeClass("hidden");
+  metadata.removeClass("hidden");
+  $(".arrowbtn").removeClass("hidden");
+  $(".backbtn").removeClass("hidden");
+  $(".zoombtn").addClass("hidden");
+});
+
+$(".backbtn").on("click", function(){
+    $(".popup-container").addClass("hidden");
+    $(".main").removeClass("hidden");
+});
+
+$(".next").on("click", function(){
+    if(currentIndex < data.length-1)
+        currentIndex+=1;
+    else
+        currentIndex = 0;
+        
+    $(".popup-container .container").fadeOut(250, function(){
+        showImgAndData(data[currentIndex]);
+        $(this).fadeIn(250, function(){
+            showImgAndData(data[currentIndex]);
+        });
+    });
+});
+        
+$(".prev").on("click", function(){
+    if(currentIndex > 0)
+        currentIndex-=1;
+    else
+        currentIndex = data.length-1;
+        
+    $(".popup-container .container").fadeOut(250, function(){
+        showImgAndData(data[currentIndex]);
+        $(this).fadeIn(250, function(){
+            showImgAndData(data[currentIndex]);
+        });
+    });
+});
+
+d3.select(".zoom-in-sub")
+    .on("click", function(){
+        scaleSub *= 1.5;
+        svgSub.transition()
+          .duration(750)
+          .call(zoomSub.scaleTo, scaleSub);
+    });
+   
+d3.select(".zoom-out-sub")
+    .on("click", function(){
+        scaleSub *= 1/1.5;
+        svgSub.transition()
+          .duration(750)
+          .call(zoomSub.scaleTo, scaleSub);
+    });
+    
+$(window).resize(function(){
+  if($(window).width() < 600){
+    imgContainerWidth = imgContainer.width();
+    height = imgContainer.height();
+    svgSub.attr("width", imgContainerWidth);
+    svgSub.attr("height", imgContainerHeight);
+    img.attr("width", imgContainerWidth);
+    img.attr("height", imgContainerHeight);
+  }else{
+    imgContainerWidth = imgContainer.width();
+    height = imgContainer.height();
+    svgSub.attr("width", imgContainerWidth);
+    svgSub.attr("height", imgContainerHeight);
+    img.attr("width", imgContainerWidth);
+    img.attr("height", imgContainerHeight);
+  }
+});
+
+function showImgAndData(d){
+    image.attr("src", d.image);
+    img.attr("xlink:href", d.image);
+    $(".poem").html("<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, ipsum est sit tenetur fuga doloremque ducimus officia nostrum id vel.</p>"); //d.poem
+    $(".artist span").text(d.artist);
+    $(".desc span").text(d.description);
 }
